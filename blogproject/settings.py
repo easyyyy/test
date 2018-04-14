9da6060839601dada86e60c403f3d142afd1324d
easyyyy <31660801+easyyyy@users.noreply.github.com>
2018-04-14 20:51:43 +0800

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = '4cyd6tklqbtzd_x%d7xxgrs97io*y#%r8u@9zb+@ik%b65$x)$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS =['127.0.0.1', 'localhost ', '.linhanyao.xyz', '39.107.237.21']
+ALLOWED_HOSTS =['127.0.0.1', 'localhost ', '.linhanyao.xyz', '39.107.237.215', '171.34.223.53']
 
 
 # Application definition
